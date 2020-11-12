1f19ac97cc0f4f7d31f0f862feb2df8415126a22
Hatim Kanchwala <hatim@hatimak.me>
2020-11-12 15:47:16 +0100

    Add initial barebones VHLS project

---
+++ examples/RLC/FpgaRLCSolver/vc707/script.tcl
@@ -0,0 +1,14 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project FpgaRLCSolver
+open_solution "vc707"
+set_part {xc7vx485tffg1761-2} -tool vivado
+create_clock -period 50ns -name default
+#source "./FpgaRLCSolver/vc707/directives.tcl"
+#csim_design
+csynth_design
+#cosim_design
+export_design -format ip_catalog
